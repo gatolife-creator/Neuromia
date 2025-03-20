46c787482a415cb66105673ac49367083187ecb4
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2025-03-20 15:49:55 +0900

    add: flashcard editing form

---
+++ app/materials/create/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function CreateMaterialPage() {
+  const form = useForm();
+
+  const onSubmit = (e) => {};
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">新しい教材を作成する</h1>
+      <Input placeholder="タイトルを入力してください"></Input>
+      <Input placeholder="説明文を入力してください"></Input>
+      <div className="my-10 bg-slate-100 border border-black rounded-lg p-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormLabel className="text-2xl font-bold">カードを作成する</FormLabel>
+            <FormField
+              control={form.control}
+              name="front"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      placeholder="問題文を入力してください"
+                      className="bg-white"
+                      {...field}
+                    ></Textarea>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <FormField
+              control={form.control}
+              name="back"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      placeholder="答えを入力してください"
+                      className="bg-white"
+                      {...field}
+                    ></Textarea>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <Button type="submit" className="cursor-pointer">追加</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
