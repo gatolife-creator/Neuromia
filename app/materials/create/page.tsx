041fd3864b214af01ffde21ade94e99fc1c77dc4
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2025-03-22 08:58:18 +0900

    refactored

--- app/materials/create/page.tsx
+++ app/materials/create/page.tsx
@@ -1,17 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Textarea } from "@/components/ui/textarea";
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
 import { EditingFlashcardList } from "../../../components/editing-flashcard-list";
 import { CardData, MaterialMetaData } from "@/lib/interfaces";
 import { materialDB } from "@/lib/db";
@@ -20,10 +9,9 @@ import { EditingFlashcardForm } from "../../../components/editing-flashcard-form
 
 export default function CreateMaterialPage() {
   const [cards, setCards] = useState<CardData[]>([]);
-  const cardCreationForm = useForm();
 
-  const onCardCreationFormSubmit = () => {
-    const { front, back } = cardCreationForm.getValues() as CardData;
+  const onCardCreationFormSubmit = (values: CardData) => {
+    const { front, back } = values;
     setCards([...cards, { front, back }]);
   };
 
@@ -47,57 +35,10 @@ export default function CreateMaterialPage() {
   return (
     <div>
       <EditingFlashcardForm
-        onMaterialCreationFormSubmit={(values) => {
-          onMaterialCreationFormSubmit(values);
-        }}
+        onCardCreationFormSubmit={onCardCreationFormSubmit}
+        onMaterialCreationFormSubmit={onMaterialCreationFormSubmit}
       />
-      
-      <div className="my-10 bg-slate-100 border border-black rounded-lg p-4">
-        <Form {...cardCreationForm}>
-          <form
-            onSubmit={cardCreationForm.handleSubmit(onCardCreationFormSubmit)}
-          >
-            <FormLabel className="text-2xl font-bold">
-              カードを作成する
-            </FormLabel>
-            <FormField
-              control={cardCreationForm.control}
-              name="front"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea
-                      placeholder="問題文を入力してください"
-                      className="bg-white"
-                      {...field}
-                    ></Textarea>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            ></FormField>
-            <FormField
-              control={cardCreationForm.control}
-              name="back"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea
-                      placeholder="答えを入力してください"
-                      className="bg-white"
-                      {...field}
-                    ></Textarea>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            ></FormField>
-            <Button type="submit" className="cursor-pointer">
-              追加
-            </Button>
-          </form>
-        </Form>
-      </div>
+
       <EditingFlashcardList cards={cards} onClickDelete={onClickDelete} />
     </div>
   );
--- components/editing-flashcard-form.tsx
+++ components/editing-flashcard-form.tsx
@@ -10,62 +10,116 @@ import {
 } from "./ui/form";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
-import { MaterialData } from "@/lib/interfaces";
+import { CardData, MaterialData } from "@/lib/interfaces";
 
 interface EditingFlashcardFormProps {
+  onCardCreationFormSubmit: (values: CardData) => void;
   onMaterialCreationFormSubmit: (values: MaterialData) => void;
 }
 
 export function EditingFlashcardForm({ ...props }: EditingFlashcardFormProps) {
-  const { onMaterialCreationFormSubmit } = props;
+  const { onCardCreationFormSubmit, onMaterialCreationFormSubmit } = props;
   const materialCreationForm = useForm();
+  const cardCreationForm = useForm();
 
   return (
-    <Form {...materialCreationForm}>
-      <form
-        onSubmit={materialCreationForm.handleSubmit(() => {
-          onMaterialCreationFormSubmit(
-            materialCreationForm.getValues() as MaterialData
-          );
-        })}
-      >
-        <FormLabel className="text-2xl font-bold">
-          新しい教材を作成する
-        </FormLabel>
-        <FormField
-          control={materialCreationForm.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  placeholder="タイトルを入力してください"
-                  {...field}
-                ></Input>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        ></FormField>
-        <FormField
-          control={materialCreationForm.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Textarea
-                  placeholder="説明文を入力してください"
-                  {...field}
-                ></Textarea>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        ></FormField>
-        <Button type="submit" className="cursor-pointer">
-          作成
-        </Button>
-      </form>
-    </Form>
+    <>
+      <Form {...materialCreationForm}>
+        <form
+          onSubmit={materialCreationForm.handleSubmit(() => {
+            onMaterialCreationFormSubmit(
+              materialCreationForm.getValues() as MaterialData
+            );
+          })}
+        >
+          <FormLabel className="text-2xl font-bold">
+            新しい教材を作成する
+          </FormLabel>
+          <FormField
+            control={materialCreationForm.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="タイトルを入力してください"
+                    {...field}
+                  ></Input>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <FormField
+            control={materialCreationForm.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    placeholder="説明文を入力してください"
+                    {...field}
+                  ></Textarea>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+          <Button type="submit" className="cursor-pointer">
+            作成
+          </Button>
+        </form>
+      </Form>
+      <div className="my-10 bg-slate-100 border border-black rounded-lg p-4">
+        <Form {...cardCreationForm}>
+          <form
+            onSubmit={cardCreationForm.handleSubmit(() => {
+              onCardCreationFormSubmit(
+                cardCreationForm.getValues() as CardData
+              );
+            })}
+          >
+            <FormLabel className="text-2xl font-bold">
+              カードを作成する
+            </FormLabel>
+            <FormField
+              control={cardCreationForm.control}
+              name="front"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      placeholder="問題文を入力してください"
+                      className="bg-white"
+                      {...field}
+                    ></Textarea>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <FormField
+              control={cardCreationForm.control}
+              name="back"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      placeholder="答えを入力してください"
+                      className="bg-white"
+                      {...field}
+                    ></Textarea>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <Button type="submit" className="cursor-pointer">
+              追加
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </>
   );
 }
