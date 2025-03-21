89952d1cd9238f04dd3034a2942d50534d1e1c58
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2025-03-21 20:04:37 +0900

    fix: UI of editing flashcard component

--- components/editing-flashcard.tsx
+++ components/editing-flashcard.tsx
@@ -1,28 +1,45 @@
+import { Button } from "./ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Textarea } from "./ui/textarea";
+import { Trash2 } from "lucide-react";
 
 interface EditingFlashcardProps {
   front: string;
   back: string;
   index: number;
+  onClickDelete: () => void;
 }
 
 export function EditingFlashcard({ ...props }: EditingFlashcardProps) {
-  const { front, back, index } = props;
+  const { front, back, index, onClickDelete } = props;
+
   return (
-    <div className="bg-slate-100">
-      <div className="bg-gray-500 text-2xl">{index}</div>
-      <div className="grid grid-cols-2 gap-4 my-5 ">
+    <Card className="w-full my-5">
+      <CardHeader>
+        <CardTitle className="flex justify-between">
+          <div>{index}</div>
+          <Button
+            className="cursor-pointer"
+            variant="destructive"
+            onClick={onClickDelete}
+          >
+            <Trash2 />
+          </Button>
+        </CardTitle>
+      </CardHeader>
+
+      <CardContent className="grid grid-cols-2 gap-4">
         <Textarea
-          className="inline-block resize-none"
+          className="w-full"
           placeholder="問題文を入力してください"
           defaultValue={front}
-        ></Textarea>
+        />
         <Textarea
-          className="inline-block resize-none"
+          className="w-full"
           placeholder="答えを入力してください"
           defaultValue={back}
-        ></Textarea>
-      </div>
-    </div>
+        />
+      </CardContent>
+    </Card>
   );
 }
