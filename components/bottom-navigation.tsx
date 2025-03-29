77982e6651f979a0fecf586eeea7c97cbd2da6eb
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2025-03-29 23:59:39 +0900

    feat: enhance bottom navigation with new add functionality and improved layout

--- components/bottom-navigation.tsx
+++ components/bottom-navigation.tsx
@@ -1,22 +1,74 @@
 import Link from "next/link";
-import { Book, Home, UserRound, Settings } from "lucide-react";
+import { Book, Home, UserRound, Settings, PlusCircle } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Button } from "./ui/button";
 
 export function BottomNavigation() {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
-    <div className="bg-primary fixed right-0 bottom-0 grid h-14 w-full grid-cols-4 border-t sm:hidden">
-      <Link href="/" className="bg-white">
+    <div className="fixed right-0 bottom-0 grid h-14 w-full grid-cols-5 gap-3 border-t bg-white sm:hidden">
+      <Link
+        href="/"
+        className={`rounded-md ${pathname === "/" ? "bg-muted" : "bg-white"}`}
+      >
         <Home className="mx-auto h-8" fontSize="32px" />
         <div className="h-6 w-full text-center text-xs">ホーム</div>
       </Link>
-      <Link href="/materials" className="bg-white">
+      <Link
+        href="/materials"
+        className={`rounded-md ${pathname === "/materials" ? "bg-muted" : "bg-white"}`}
+      >
         <Book className="mx-auto h-8" fontSize="32px" />
         <div className="h-6 w-full text-center text-xs">教材一覧</div>
       </Link>
-      <Link href="/groups" className="bg-white">
+      <Sheet>
+        <SheetTrigger>
+          <PlusCircle className="mx-auto h-8" fontSize="32px" />
+          <div className="h-6 w-full text-center text-xs">追加</div>
+        </SheetTrigger>
+        <SheetContent className="w-[400px] sm:w-[540px]" side="bottom">
+          <SheetHeader>
+          </SheetHeader>
+          <div className="mx-auto min-h-[100px] w-4/5 space-y-1">
+            <Button
+              className="w-full cursor-pointer"
+              onClick={() => {
+                router.push("/materials/create");
+              }}
+            >
+              新しい教材を作成する
+            </Button>
+            <Button
+              className="w-full cursor-pointer"
+              onClick={() => {
+                router.push("#");
+              }}
+            >
+              新しいグループを作成する
+            </Button>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+      <Link
+        href="/groups"
+        className={`rounded-md ${pathname === "/groups" ? "bg-muted" : "bg-white"}`}
+      >
         <UserRound className="mx-auto h-8" fontSize="32px" />
         <div className="h-6 w-full text-center text-xs">グループ</div>
       </Link>
-      <Link href="/settings" className="bg-white">
+      <Link
+        href="/settings"
+        className={`rounded-md ${pathname === "/settings" ? "bg-muted" : "bg-white"}`}
+      >
         <Settings className="mx-auto h-8" fontSize="32px" />
         <div className="h-6 w-full text-center text-xs">設定</div>
       </Link>
