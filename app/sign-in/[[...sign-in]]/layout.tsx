ddd4a03660bdc6d075ee2f9a124177d76c764221
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2025-03-30 11:33:41 +0900

    feat: implement sign-in layout

---
+++ app/sign-in/[[...sign-in]]/layout.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import type React from "react";
+// import { ThemeProvider } from "@/components/theme-provider";
+
+import "@/app/globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { jaJP } from "@clerk/localizations";
+import { Header } from "@/components/header";
+
+export default function SignInLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider localization={jaJP}>
+      <html lang="ja">
+        <head>
+          <title>Neuromia</title>
+        </head>
+        <body>
+          {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem> */}
+          <Header />
+          {children}
+          {/* </ThemeProvider> */}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
